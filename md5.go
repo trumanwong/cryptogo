5eff6d512bfde8fe972b1785f810ed8288736e89
trumanwong <1415829005@qq.com>
2023-10-19 10:53:22 +0800

    fix md5

--- md5.go
+++ md5.go
@@ -9,38 +9,38 @@ import (
 )
 
 // MD5 return md5 encrypted string
-func MD5(clearText string) string {
-	return fmt.Sprintf("%x", md5.Sum([]byte(clearText)))
+func MD5(clearText []byte) string {
+	return fmt.Sprintf("%x", md5.Sum(clearText))
 }
 
 // MD5ToUpper Return MD5 uppercase encrypted string
-func MD5ToUpper(clearText string) string {
+func MD5ToUpper(clearText []byte) string {
 	return strings.ToUpper(MD5(clearText))
 }
 
 // MD5ToLower Return MD5 lowercase encrypted string
-func MD5ToLower(clearText string) string {
+func MD5ToLower(clearText []byte) string {
 	return MD5(clearText)
 }
 
 // MD5Sixteen Returns a 16-bit MD5 encrypted string
-func MD5Sixteen(clearText string) string {
+func MD5Sixteen(clearText []byte) string {
 	return MD5(clearText)[8:24]
 }
 
 // MD5SixteenToUpper Returns a 16-digit uppercase MD5 encrypted string
-func MD5SixteenToUpper(clearText string) string {
+func MD5SixteenToUpper(clearText []byte) string {
 	return strings.ToUpper(MD5Sixteen(clearText))
 }
 
 // MD5SixteenToLower Returns a 16-digit uppercase MD5 encrypted string
-func MD5SixteenToLower(clearText string) string {
+func MD5SixteenToLower(clearText []byte) string {
 	return MD5Sixteen(clearText)
 }
 
 // HmacMD5 Keyed-hash message authentication codes (HMAC) is a mechanism for message authentication using cryptographic hash functions.
-func HmacMD5(key, clearText string) string {
-	h := hmac.New(md5.New, []byte(key))
-	h.Write([]byte(clearText))
+func HmacMD5(key, clearText []byte) string {
+	h := hmac.New(md5.New, key)
+	h.Write(clearText)
 	return hex.EncodeToString(h.Sum(nil))
 }
--- md5_test.go
+++ md5_test.go
@@ -7,63 +7,63 @@ import (
 )
 
 func ExampleMD5() {
-	fmt.Println(MD5("123"))
+	fmt.Println(MD5([]byte("123")))
 	// Output: 202cb962ac59075b964b07152d234b70
 }
 
 func TestMD5(t *testing.T) {
-	assert.Equal(t, "202cb962ac59075b964b07152d234b70", MD5("123"))
+	assert.Equal(t, "202cb962ac59075b964b07152d234b70", MD5([]byte("123")))
 }
 func ExampleMD5ToLower() {
-	fmt.Println(MD5ToLower("123"))
+	fmt.Println(MD5ToLower([]byte("123")))
 	// Output: 202cb962ac59075b964b07152d234b70
 }
 
 func TestMD5ToLower(t *testing.T) {
-	assert.Equal(t, "202cb962ac59075b964b07152d234b70", MD5ToLower("123"))
+	assert.Equal(t, "202cb962ac59075b964b07152d234b70", MD5ToLower([]byte("123")))
 }
 
 func ExampleMD5ToUpper() {
-	fmt.Println(MD5ToUpper("123"))
+	fmt.Println(MD5ToUpper([]byte("123")))
 	// Output: 202CB962AC59075B964B07152D234B70
 }
 
 func TestMD5ToUpper(t *testing.T) {
-	assert.Equal(t, "202CB962AC59075B964B07152D234B70", MD5ToUpper("123"))
+	assert.Equal(t, "202CB962AC59075B964B07152D234B70", MD5ToUpper([]byte("123")))
 }
 
 func ExampleMD5Sixteen() {
-	fmt.Println(MD5Sixteen("123"))
+	fmt.Println(MD5Sixteen([]byte("123")))
 	// Output: ac59075b964b0715
 }
 
 func TestMD5Sixteen(t *testing.T) {
-	assert.Equal(t, "ac59075b964b0715", MD5Sixteen("123"))
+	assert.Equal(t, "ac59075b964b0715", MD5Sixteen([]byte("123")))
 }
 
 func ExampleMD5SixteenToUpper() {
-	fmt.Println(MD5SixteenToUpper("123"))
+	fmt.Println(MD5SixteenToUpper([]byte("123")))
 	// Output: AC59075B964B0715
 }
 
 func TestMD5SixteenToUpper(t *testing.T) {
-	assert.Equal(t, "AC59075B964B0715", MD5SixteenToUpper("123"))
+	assert.Equal(t, "AC59075B964B0715", MD5SixteenToUpper([]byte("123")))
 }
 
 func ExampleMD5SixteenToLower() {
-	fmt.Println(MD5SixteenToLower("123"))
+	fmt.Println(MD5SixteenToLower([]byte("123")))
 	// Output: ac59075b964b0715
 }
 
 func TestMD5SixteenToLower(t *testing.T) {
-	assert.Equal(t, "ac59075b964b0715", MD5SixteenToLower("123"))
+	assert.Equal(t, "ac59075b964b0715", MD5SixteenToLower([]byte("123")))
 }
 
 func ExampleHmacMD5() {
-	fmt.Println(HmacMD5("TrumanWong", "123"))
+	fmt.Println(HmacMD5([]byte("TrumanWong"), []byte("123")))
 	// Output: 1a152759b05a1cc0e2841a7fb58a559b
 }
 
 func TestHmacMD5(t *testing.T) {
-	assert.Equal(t, "c8ec4ed8338e4d0a81e75ba3b9d290a8", HmacMD5("", "123"))
+	assert.Equal(t, "c8ec4ed8338e4d0a81e75ba3b9d290a8", HmacMD5([]byte(""), []byte("123")))
 }
